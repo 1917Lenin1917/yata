3b89dab6de73376dabd34a0c6d0c73190bd6689d
Maksim Litvinenko <mlitvinenko@p.systems>
2025-10-02 19:19:12 +0200

    feat: remove some unused modules.

--- src/components/tiptap-templates/simple/simple-editor.tsx
+++ src/components/tiptap-templates/simple/simple-editor.tsx
@@ -14,7 +14,6 @@ import { Image } from "@tiptap/extension-image";
 import { TaskItem, TaskList } from "@tiptap/extension-list";
 import { TextAlign } from "@tiptap/extension-text-align";
 import { Typography } from "@tiptap/extension-typography";
-import { Highlight } from "@tiptap/extension-highlight";
 import { Subscript } from "@tiptap/extension-subscript";
 import { Superscript } from "@tiptap/extension-superscript";
 import { Selection } from "@tiptap/extensions";
@@ -25,10 +24,8 @@ import { Toolbar, ToolbarGroup, ToolbarSeparator } from "../../ui/toolbar";
 
 // --- Tiptap Node ---
 import { ImageUploadNode } from "@/components/tiptap-node/image-upload-node/image-upload-node-extension";
-import { HorizontalRule } from "@/components/tiptap-node/horizontal-rule-node/horizontal-rule-node-extension";
 import "@/components/tiptap-node/blockquote-node/blockquote-node.scss";
 import "@/components/tiptap-node/code-block-node/code-block-node.scss";
-import "@/components/tiptap-node/horizontal-rule-node/horizontal-rule-node.scss";
 import "@/components/tiptap-node/list-node/list-node.scss";
 import "@/components/tiptap-node/image-node/image-node.scss";
 import "@/components/tiptap-node/heading-node/heading-node.scss";
@@ -176,17 +173,14 @@ export function SimpleEditor({
     },
     extensions: [
       StarterKit.configure({
-        horizontalRule: false,
         link: {
           openOnClick: false,
           enableClickSelection: true,
         },
       }),
-      HorizontalRule,
       TextAlign.configure({ types: ["heading", "paragraph"] }),
       TaskList,
       TaskItem.configure({ nested: true }),
-      Highlight.configure({ multicolor: true }),
       Image,
       Typography,
       Superscript,
